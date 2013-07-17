ed1522318724f900d1270153fd7cf425a25b792c
Darescu Ionut <johnny@esi.ro>
2013-07-17 21:08:35 +0300

    update to: Linaro GCC 4.8-2013.07 (4.8.2)

--- arm-cortex_a8-linux-gnueabi/sysroot/lib/libstdc++.so.6.0.18-gdb.py
+++ arm-cortex_a8-linux-gnueabi/sysroot/lib/libstdc++.so.6.0.18-gdb.py
@@ -19,7 +19,7 @@ import gdb
 import os
 import os.path
 
-pythondir = '/media/hdd_linux/crosstool-ng/builds/arm-cortex_a8-linux-gnueabi/share/gcc-4.8.1/python'
+pythondir = '/media/hdd_linux/crosstool-ng/builds/arm-cortex_a8-linux-gnueabi/share/gcc-4.8.2/python'
 libdir = '/media/hdd_linux/crosstool-ng/builds/arm-cortex_a8-linux-gnueabi/arm-cortex_a8-linux-gnueabi/lib'
 
 # This file might be loaded when there is no current objfile.  This
